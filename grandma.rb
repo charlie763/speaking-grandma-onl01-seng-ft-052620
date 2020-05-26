e2631d6c2257185f8301e45d76d923a324db59f7
Charlie Wisoff <cwisoff@gmail.com>
2020-05-26 21:28:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,10 @@
 
 def speak_to_grandma(phrase)
     if phrase == phrase.upcase
-        return "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     elsif phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
     else
+      return "HUH?! SPEAK UP, SONNY!"
     end
 end
